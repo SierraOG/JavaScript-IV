348498fd2f3e094c4fc61cacbe0fe49a58d88c88
SierraOG <sierrao@utexas.edu>
2019-06-13 11:52:58 -0500

    added student class in lamdba-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,7 +7,7 @@ class Person{
         this.location = attributes.location;
     }
     speak(){
-        return `Hello my name is ${this.name}, I am from ${this.location}`;
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
     }
 }
 
@@ -19,10 +19,27 @@ class Instructor extends Person {
         this.catchPhrase = attributes.catchPhrase;
     }
     demo(subject){
-        return `Today we are learning about ${subject}` ;
+        console.log(`Today we are learning about ${subject}`);
     }
     grade(student,subject){
-        return `${student.name} receives a perfect score on ${subject}`;
+        console.log(`${student.name} receives a perfect score on ${subject}`);
     }
 }
 
+class Student extends Person{
+    constructor(attributes){
+        super(attributes);
+        this.previousBackground = attributes.previousBackground;
+        this.className = attributes.className;
+        this.favSubjects = attributes.favSubjects;
+    }
+    listsSubjects(){
+        console.log(...this.favLanguage);
+    }
+    PRAssignment(subject){
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    }
+    sprintChallenge(subject){
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    }
+}
